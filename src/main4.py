caa2dc215f3cd735282d327cc71e8b8b634658f9
Saksham <sakshamdam54321@gmail.com>
2020-10-18 12:14:08 +0530

    fixed week 4 assignment solution The problem was that when we were dividing the values of the two dictionary they were in string format so the error was coming. To fix that we have to typecast it into int. And I have fixed some intendation

--- src/main4.py
+++ src/main4.py
@@ -1,41 +1,41 @@
 import os
 import csv
 
-file_tag_freq=open('../generatedFiles/tag_freq.csv','r')
-file_word_freq=open('../generatedFiles/word_freq.csv','r')
-file_word_tag_freq=open('../generatedFiles/data2.csv','r')
+file_word_tag_freq = open('../generatedFiles/data2.csv', 'r')
+file_tag_freq = open('../generatedFiles/tag_freq.csv', 'r')
+file_word_freq = open('../generatedFiles/word_freq.csv', 'r')
 
-dict_tag_freq={}
-list_word_freq=[]
-dict_word_tag_freq={}
+dict_word_tag_freq = {}
+dict_tag_freq = {}
+list_word_freq = []
 
-tagfreq=csv.reader(file_tag_freq)
-wordfreq=csv.reader(file_word_freq)
-wordtagfreq=csv.reader(file_word_tag_freq)
+wordtagfreq = csv.reader(file_word_tag_freq)
+tagfreq = csv.reader(file_tag_freq)
+wordfreq = csv.reader(file_word_freq)
+
+for word_tag in wordtagfreq:
+    dict_word_tag_freq[word_tag[0]] = word_tag[1]
+
+for tag in tagfreq:
+    dict_tag_freq[tag[0]] = tag[1]
 
 for word in wordfreq:
     list_word_freq.append(word)
 
-for tag in tagfreq:
-    dict_tag_freq[tag[0]]=tag[1]
+probability_of_word_given_tag = {}
 
-for word_tag in wordtagfreq:
-    dict_word_tag_freq[word_tag[0]]=word_tag[1]
-
-probability_of_word_given_tag={}
 # P(Word|Tag) = P(Word^Tag)/P(Tag) = Count(Word_Tag)/Count(Tag)
 
 for word in list_word_freq:
     for tag in dict_tag_freq.keys():
-        temp=word[0]+"_"+tag
-        temp1=word[0]+"|"+tag
-        probability_of_word_given_tag[temp1]=dict_word_tag_freq[temp]/dict_tag_freq[tag]
-        
+        temp = word[0]+"_"+tag
+        temp1 = word[0]+"|"+tag
+        if (temp in dict_word_tag_freq):
+            probability_of_word_given_tag[temp1] = int(dict_word_tag_freq[temp]) / int(dict_tag_freq[tag])
+        else:
+            probability_of_word_given_tag[temp1] = 0
+
 print(probability_of_word_given_tag)
-
-
-
-
+file_word_tag_freq.close()
 file_tag_freq.close()
 file_word_freq.close()
-file_word_tag_freq.close()
